d98bcaa20b211b2c346f2dee59bec902c92447f6
Alexis_Jhair <alexisvillarruelj14@gmail.com>
2024-10-05 10:48:21 -0500

    mostrar lista

--- app/src/main/java/dev/alexisvillarruel/logingpcparcial_reciclyeview/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/dev/alexisvillarruel/logingpcparcial_reciclyeview/ui/gallery/GalleryFragment.kt
@@ -4,39 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import dev.alexisvillarruel.logingpcparcial_reciclyeview.R
 import dev.alexisvillarruel.logingpcparcial_reciclyeview.databinding.FragmentGalleryBinding
 
 class GalleryFragment : Fragment() {
-
-    private var _binding: FragmentGalleryBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    var arrSo = arrayOf("Manchester United", "Real Madrid", "PSG", "Bayern Munich",)
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+    ): View? {
+        val view: View = inflater.inflate(R.layout.fragment_gallery, container, false)
+        val lista : ListView = view.findViewById(R.id.lstequipos)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        val adapter: Any?=
+            ArrayAdapter<Any>(this.requireContext(), android.R.layout.simple_list_item_1, arrSo)
+            lista.adapter = adapter as ArrayAdapter<*>?
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        return view
         }
-        return root
-    }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
-}
